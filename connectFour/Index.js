59b066ace37b2238bb4d74c8b5e90f2d61493197
AlexLara-Umana <74322508+AlexLara-Umana@users.noreply.github.com>
2022-12-08 18:57:48 -0800

    Delete connectFour directory

--- connectFour/Index.js
+++
@@ -1,8 +0,0 @@
-function onPageLoad() {
-  if (localStorage.getItem("board-color") == null) {
-    localStorage.setItem("board-size", 0);
-    localStorage.setItem("board-color", 4);
-    localStorage.setItem("p0-color", 0);
-    localStorage.setItem("p1-color", 3);
-  }
-}
--- connectFour/PHP/Loggedin.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['loggedin']))
-    echo $_SESSION['loggedin'];
-else echo 0;
-?>
